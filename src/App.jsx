bbedc7c9b0b929aba3a5dd85414d1317a868a859
Joseph G Senzota <josephgenes1999@gmail.com>
2026-01-19 15:10:15 +0300

    Remove App.css import from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.css";
 import Register from "./pages/Register";
 import RegisterTenant from "./pages/tenantRegister";
 import Login from "./pages/Login";
