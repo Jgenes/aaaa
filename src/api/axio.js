3e3a2bf114cba0d9621d4622351986cda93c237a
Joseph G Senzota <josephgenes1999@gmail.com>
2026-02-28 01:27:00 -0800

    Update axio.js

--- src/api/axio.js
+++ src/api/axio.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://traininghubtz.co.tz/api",
   headers: {
     Accept: "application/json", // ✔ keep this
   },
