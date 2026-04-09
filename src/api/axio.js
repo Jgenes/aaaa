243d3b52b69012075bfcba6b5a95b069965a1931
Joseph G Senzota <josephgenes1999@gmail.com>
2026-04-09 06:32:56 -0700

    Update axio.js

--- src/api/axio.js
+++ src/api/axio.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://unsecurely-unaddressed-stepanie.ngrok-free.dev/api",
   headers: {
     Accept: "application/json", // ✔ keep this
   },
